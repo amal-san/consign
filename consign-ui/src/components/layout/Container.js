e0c98f71f71b8e3752506be52aa5643fb27590b0
amal-san <amaldemaniax@gmail.com>
2021-03-26 18:41:19 +0530

    landing page update

--- consign-ui/src/components/layout/Container.js
+++ consign-ui/src/components/layout/Container.js
@@ -4,6 +4,10 @@ const Container = styled.div`
   width:${props => props.width ? props.width : "auto"};
   height:${props => props.height ? props.height : "auto"};
   background: ${props => props.bg ? props.bg : "transparent"};
+  display:${props => props.display ? props.display : "block"};
+  justify-content:${props => props.justify ? props.justify : "start"};
+  align-items:${props => props.align ? props.align : "start"};
+
 `;
 
 
